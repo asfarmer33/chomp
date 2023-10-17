6948d59cae19c90947b9fbe2835758f0968ff26d
asfarmer33 <148174736+asfarmer33@users.noreply.github.com>
2023-10-17 08:33:35 -0400

    changes

--- game.py
+++ game.py
@@ -1,1 +1,28 @@
-print("This is a new message")
+import pygame
+from pygame.locals import *
+import sys
+
+pygame.init()
+
+HEIGHT = 800
+WIDTH = 600
+
+screen = pygame.display.set_mode((HEIGHT,WIDTH))
+FPS = pygame.time.Clock()
+
+water = pygame.Surface((HEIGHT,WIDTH))
+
+running = True
+
+while running:
+    pygame.Surface.fill(water,'blue')
+
+    for event in pygame.event.get():
+        if event.type == QUIT:
+            pygame.quit()
+            sys.exit()
+
+
+    pygame.display.flip()
+    pygame.display.update()
+    FPS.tick(60)
