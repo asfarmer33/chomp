b1961dce8b932f63f22545d51a0d93c8a31a5f89
asfarmer33 <148174736+asfarmer33@users.noreply.github.com>
2023-11-06 08:39:06 -0500

    Update fish.py

--- fish.py
+++ fish.py
@@ -17,6 +17,7 @@ class Fish(pygame.sprite.Sprite):
         self.rect.y = self.y
         self.screen = screen
         self.dead_timer = 0
+        self.deadspeed = 0
         self.score = score # list with one item
 
     def update(self):
@@ -24,18 +25,23 @@ class Fish(pygame.sprite.Sprite):
             self.x = self.screen.get_width()
             self.y = random.randint(200, self.screen.get_height()-self.image.get_height())
         self.x -= self.speed
+        self.y -= self.deadspeed
         self.rect.x = self.x
         self.rect.y = self.y
-        if self.dead_timer and pygame.time.get_ticks() - self.dead_timer > 300:
+        if self.dead_timer:
+            opacity = (pygame.time.get_ticks() - self.dead_timer)/100
+            self.image.set_alpha((1/opacity)*100)
+        if self.dead_timer and pygame.time.get_ticks() - self.dead_timer > 800:
             self.kill()
 
     def skeleton(self):
         if self.speed != 0:
             dead_imgs = ["images/fishTile_097.png", "images/fishTile_099.png", "images/fishTile_093.png", "images/fishTile_095.png"]
             self.image = pygame.image.load(dead_imgs[self.randimg])
-            self.image = pygame.transform.flip(self.image, 1, 0)
+            self.image = pygame.transform.flip(self.image, 1, 1)
             self.dead_timer = pygame.time.get_ticks()
             self.speed = 0
+            self.deadspeed = 1
 
             self.score[0] += 1
 
